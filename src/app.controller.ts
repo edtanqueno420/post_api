a64760bfc783544746893c75f4cab22c867aa2e8
edtanqueno420 <edison.tanqueno@gmail.com>
2025-12-17 21:49:17 -0500

    added health endpoint1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  
-
-  @Get('health')
+  @Get()
   health():any {
     return this.appService.getHealth();
    }
